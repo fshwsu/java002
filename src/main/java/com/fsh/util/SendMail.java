977f6ace63e4e78e80e8485c6779e5c4705338f6
fshwsu <fshwsu@126.com>
2018-07-18 22:18:53 +0800

    首次初始化提交代码

---
+++ src/main/java/com/fsh/util/SendMail.java
@@ -0,0 +1,29 @@
+package com.fsh.util;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+public class SendMail {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-mail.xml");
+
+		JavaMailSenderImpl	mailsend=(JavaMailSenderImpl) context.getBean("mailSender");
+	
+		//创建简单的邮件
+		SimpleMailMessage msg = new SimpleMailMessage();
+		msg.setFrom("fshwsu@126.com");
+		msg.setTo("951031232@qq.com");
+		msg.setSubject("JAVA0115测试邮件");
+		msg.setText("好好学习,天天向上!");
+		
+		//发送邮件
+		
+		mailsend.send(msg);
+		
+		System.out.println("send ok");
+
+	}
+
+}
